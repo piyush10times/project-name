4fac90c621a8123c45fc6f46d0160899c61e79af
piyush10times <piyush@10times.com>
2024-08-22 20:48:32 +0530

    yyyyy

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { CorsGuard } from './corsgaurd';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalGuards(new CorsGuard());
-  app.enableCors({
-    credentials: true,
-    allowedHeaders: 'Content-Type,Authorization',
-  })
+  // app.useGlobalGuards(new CorsGuard());
+  // app.enableCors({
+  //   credentials: true,
+  //   allowedHeaders: 'Content-Type,Authorization',
+  // });
   await app.listen(7878);
 }
 bootstrap();
